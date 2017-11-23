fd9a963423f186292d5b56f7895399e2c30618f8
Nandan Mankad <mankadnandan@gmail.com>
2017-11-23 22:27:29 +0530

    Life, Universe and Everything for Basics Of IO CondeMonk HackerEarth.

--- src/codemonk/basicsofprogramming/basicsofio/examples/LifeUniverseAndEverything.java
+++ src/codemonk/basicsofprogramming/basicsofio/examples/LifeUniverseAndEverything.java
@@ -3,7 +3,22 @@ package codemonk.basicsofprogramming.basicsofio.examples;
 import java.util.Scanner;
 
 /**
+ *
+ * SOLVED SUCCESSFULLY
+ *
  * Created by Nandan Mankad on 22-Nov-17.
+ *
+ * #####################################################################################################################################################################
+ * Approach: Write each number from input to output. Stop when read 42.
+ *
+ * Time Complexity: O(N)
+ * #####################################################################################################################################################################
+ *
+ Your program is to use the brute-force approach in order to find the Answer to Life, the Universe, and Everything.
+ More precisely... rewrite small numbers from input to output. Stop processing input after reading in the number 42.
+ All numbers at input are integers of one or two digits.
+ *
+ *
  */
 public class LifeUniverseAndEverything {
     public static void main(String args[] ) throws Exception {
