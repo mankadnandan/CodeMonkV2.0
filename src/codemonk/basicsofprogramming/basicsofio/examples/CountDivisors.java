a55f833d02d069c9939d89df78c3d79638a40dc9
Nandan Mankad <mankadnandan@gmail.com>
2017-11-23 21:50:28 +0530

    Count Divisor for Basics Of IO CondeMonk HackerEarth.

--- src/codemonk/basicsofprogramming/basicsofio/examples/CountDivisors.java
+++ src/codemonk/basicsofprogramming/basicsofio/examples/CountDivisors.java
@@ -3,7 +3,30 @@ package codemonk.basicsofprogramming.basicsofio.examples;
 import java.util.Scanner;
 
 /**
+ *
+ * SOLVED SUCCESSFULLY
+ *
  * Created by Nandan Mankad on 22-Nov-17.
+ *
+ * #####################################################################################################################################################################
+ * Approach: We iterate from l to r and see if each number is divisible by k.
+ *
+ * Time Complexity: O(N) where N = l to r.
+ * #####################################################################################################################################################################
+ *
+ You have been given 3 integers - l, r and k. Find how many numbers between l and r (both inclusive) are divisible by k.
+ You do not need to print these numbers, you just have to find their count.
+
+ Input Format
+ The first and only line of input contains 3 space separated integers l, r and k.
+
+ Output Format
+ Print the required answer on a single line.
+
+ Constraints:
+ 1 ≤ l ≤ r ≤ 1000
+ 1 ≤ k ≤ 1000
+ *
  */
 public class CountDivisors {
     public static void main(String args[] ) throws Exception {
